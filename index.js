5a047de020ed23fad858edc98e16016df8f93372
Subhajit Sahu <wolfram77@gmail.com>
2018-06-05 15:31:29 +0530

    :snowman: lazy corpus load

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const columns = require('@ifct2017/columns');
 const path = require('path');
-const corpus = require('./corpus');
+
+var corpus = new Map();
+var ready = false;
+
+
+function load() {
+  if(ready) return true;
+  for(var [k, v] of require('./corpus'))
+    corpus.set(k, v);
+  return ready = true;
+};
 
 function nutrients(txt) {
+  if(!ready) return null;
   var mats = columns(txt);
   if(mats.length===0) return null;
   return corpus.get(mats[0].code)||null;
 };
+nutrients.load = load;
 nutrients.corpus = corpus;
 module.exports = nutrients;
