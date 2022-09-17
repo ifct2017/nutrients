eccb6c7a75376fa802cab4df6995439760fa0089
Subhajit Sahu <wolfram77@gmail.com>
2022-09-17 14:00:20 +0530

    :mango: update build setup

--- build.js
+++ build.js
@@ -1,9 +1,11 @@
-const fs   = require('fs');
-const os   = require('os');
-const path = require('path');
-const lunr = require('lunr');
+const path  = require('path');
+const fs    = require('extra-fs');
+const build = require('extra-build');
+const lunr  = require('lunr');
 const columns = require('@ifct2017/columns');
 
+const owner = 'ifct2017';
+const repo  = build.readMetadata('.').name.replace(/@.+\//g, '');
 const OVERRIDE = new Map([
   ['tocpha', 'vitamin-e'],
   ['tocphd', 'vitamin-e'],
@@ -18,26 +20,15 @@ const OVERRIDE = new Map([
 
 
 
-function readFile(pth) {
-  var d = fs.readFileSync(pth, 'utf8');
-  return d.replace(/\r?\n/g, '\n');
-}
-
-function writeFile(pth, d) {
-  d = d.replace(/\r?\n/g, os.EOL);
-  fs.writeFileSync(pth, d);
-}
-
-
-function matchIndex(idx, txt) {
+function matchIndex(index, txt) {
   var a  = [], txt = txt.replace(/\W/g, ' ');
-  var ms = idx.search(txt), max = 0;
+  var ms = index.search(txt), max = 0;
   for (var m of ms)
     max = Math.max(max, Object.keys(m.matchData.metadata).length);
   for (var m of ms) {
     var keys = Object.keys(m.matchData.metadata).length;
     if (keys<max) continue;
-    if (!m.ref.includes('+') && max < m.ref.split('-').length) continue;
+    if (!m.ref.includes('+') && max<m.ref.split('-').length) continue;
     a.push(m);
   }
   return a;
@@ -49,7 +40,7 @@ function readAssets() {
     var pth  = path.join('assets', f);
     var code = f.replace('.txt', '');
     var tags = code.replace(/\W+/g, ' ');
-    var desc = readFile(pth).replace(/[\n\s]+$/, '');
+    var desc = fs.readFileTextSync(pth).replace(/[\n\s]+$/, '');
     a.set(code, {code, tags, desc});
   }
   return a;
@@ -65,12 +56,12 @@ function indexAssets(map) {
   });
 }
 
-function createTable(idx) {
+function createTable(index) {
   var a = new Map();
   for (var c of columns.load().values()) {
     var cname = c.name.replace(/\W+/g, ' ').toLowerCase();
-    var tags = `${c.code} ${c.code} ${c.code} ${cname} ${cname} ${c.tags}`;
-    var ms = matchIndex(idx, tags);
+    var tags  = `${c.code} ${c.code} ${c.code} ${cname} ${cname} ${c.tags}`;
+    var ms    = matchIndex(index, tags);
     // if (c.code==='cartb') console.log(tags, ms);
     if (ms.length===0) console.log('Skipped:', c.name, tags, ms);
     if (ms.length===0) continue;
@@ -81,19 +72,61 @@ function createTable(idx) {
   return a;
 }
 
-function writeCorpus(ast, tab) {
+function writeCorpus(assets, table) {
   var a = '';
-  for (var [k, v] of ast)
+  for (var [k, v] of assets)
     a += `const ${k.replace(/\W+/g, '_')} = ${JSON.stringify(v.desc)};\n`;
   a += `const CORPUS = new Map([\n`;
-  for (var [k, v] of tab)
+  for (var [k, v] of table)
     a += `  ["${k}", ${v.replace(/\W+/g, '_')}],\n`;
   a += `]);\n`;
   a += `module.exports = CORPUS;\n`;
-  writeFile('corpus.js', a);
+  fs.writeFileTextSync('corpus.js', a);
 }
 
-var ast = readAssets();
-var idx = indexAssets(ast);
-var tab = createTable(idx);
-writeCorpus(ast, tab);
+function generateCorpus() {
+  var assets = readAssets();
+  var index  = indexAssets(assets);
+  var table  = createTable(index);
+  writeCorpus(assets, table);
+}
+
+
+
+
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ver) {
+  var _package = build.readDocument('package.json');
+  var m = build.readMetadata('.');
+  m.version = ver;
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
+}
+
+
+// Publish root, sub packages to NPM, GitHub.
+function publishPackages() {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  generateCorpus();
+  publishRootPackage(ver);
+}
+
+
+// Publish docs.
+function publishDocs() {
+  var m = build.readMetadata('.');
+  build.updateGithubRepoDetails({owner, repo, topics: m.keywords});
+}
+
+
+// Finally.
+function main(a) {
+  if (a[2]==='publish-docs') publishDocs();
+  else if (a[2]==='publish-packages') publishPackages();
+  else generateCorpus();
+}
+main(process.argv);
